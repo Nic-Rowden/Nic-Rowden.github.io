19e82ff058b9bd72e87c4f2715ea53eb91ea0eec
Nic-Rowden <141164422+Nic-Rowden@users.noreply.github.com>
2023-08-31 18:56:57 -0400

    Update searchresults.js

--- sandbox/searchresults.js
+++ sandbox/searchresults.js
@@ -1,29 +1,21 @@
 // Get references to the elements
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
+const galleryImages = document.querySelectorAll('.gallery-img');
 
 // Attach event listener to the form submission
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent the form from actually submitting
     
-    const searchTerm = searchInput.value.trim();
-    
-    if (searchTerm !== '') {
-        // Redirect to the results page with the search term as a query parameter
-        window.location.href = `search-results.html?query=${encodeURIComponent(searchTerm)}`;
-    }
+    const searchTerm = searchInput.value.trim().toLowerCase();
+
+    // Loop through each gallery image and hide/show based on search term
+    galleryImages.forEach((image) => {
+        const imageName = image.querySelector('.projectname').textContent.toLowerCase();
+        if (imageName.includes(searchTerm)) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
 });
-// Get reference to the results container
-const resultsContainer = document.getElementById('resultsContainer');
-
-// Get the search term from the URL query parameter
-const queryParams = new URLSearchParams(window.location.search);
-const searchTerm = queryParams.get('query');
-
-if (searchTerm) {
-    // Perform your search logic here and populate the resultsContainer
-    // For demonstration purposes, let's just display the search term
-    resultsContainer.textContent = `Search results for: ${searchTerm}`;
-} else {
-    resultsContainer.textContent = "No search term provided.";
-}
