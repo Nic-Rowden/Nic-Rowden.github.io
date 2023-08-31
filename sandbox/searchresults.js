4870f337c742c39e2cb43b94f7029a53dc97aedc
Nic-Rowden <141164422+Nic-Rowden@users.noreply.github.com>
2023-08-31 19:14:23 -0400

    Update searchresults.js

--- sandbox/searchresults.js
+++ sandbox/searchresults.js
@@ -1,21 +1,31 @@
 // Get references to the elements
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
-const galleryImages = document.querySelectorAll('.gallery-img');
+const galleryItems = document.querySelectorAll('.gallery-img');
 
 // Attach event listener to the form submission
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent the form from actually submitting
-    
+    performSearch();
+});
+
+// Attach event listener to the Enter key press in the search input
+searchInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        performSearch();
+    }
+});
+
+function performSearch() {
     const searchTerm = searchInput.value.trim().toLowerCase();
 
-    // Loop through each gallery image and hide/show based on search term
-    galleryImages.forEach((image) => {
-        const imageName = image.querySelector('.projectname').textContent.toLowerCase();
-        if (imageName.includes(searchTerm)) {
-            image.style.display = 'block';
+    // Loop through each gallery item and hide/show based on search term
+    galleryItems.forEach((item) => {
+        const itemName = item.querySelector('.projectname').textContent.toLowerCase();
+        if (itemName.includes(searchTerm)) {
+            item.style.display = 'block';
         } else {
-            image.style.display = 'none';
+            item.style.display = 'none';
         }
     });
-});
+}
