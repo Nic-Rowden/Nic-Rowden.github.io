7ea51c99558417aae5ae2e63634c230c8084c1c2
Nic-Rowden <nicole.rowden@first.edu>
2023-09-02 12:54:55 -0400

    fixed tags in 'about' and updated javascript, clear button added.

--- sandbox/searchresults.js
+++ sandbox/searchresults.js
@@ -2,19 +2,6 @@
 const queryParams = new URLSearchParams(window.location.search);
 const searchTerm = queryParams.get('query');
 
-// Get references to the gallery items
-const galleryItems = document.querySelectorAll('.gallery-img');
-
-// Loop through each gallery item and hide/show based on search term
-galleryItems.forEach((item) => {
-    const itemName = item.querySelector('.projectname').textContent.toLowerCase();
-    if (itemName.includes(searchTerm)) {
-        item.style.display = 'block';
-    } else {
-        item.style.display = 'none';
-    }
-});
-
 // Assuming you have an input element with an id of "searchInput"
 const searchInput = document.getElementById("searchInput");
 
@@ -26,7 +13,45 @@ searchInput.addEventListener("keyup", function(event) {
 
 function performSearch() {
   const newSearchTerm = searchInput.value.trim().toLowerCase();
-  
-  // Redirect to the search results page with the new search term as a query parameter
-  window.location.href = `search-results.html?query=${encodeURIComponent(newSearchTerm)}`;
+
+  // Get the current page URL
+  const currentPage = window.location.pathname;
+
+  if (currentPage === '/search-results.html') {
+    // If the current page is the search results page, perform filtering or any other actions
+    filterResults(newSearchTerm);
+  } else {
+    // Redirect to the search results page with the new search term as a query parameter
+    window.location.href = `search-results.html?query=${encodeURIComponent(newSearchTerm)}`;
+  }
+}
+
+// Get references to the gallery items
+const galleryItems = document.querySelectorAll('.gallery-img');
+
+// Assuming you have an element with an id of "clearButton"
+const clearButton = document.getElementById("clearButton");
+
+// Add event listener for the clear button
+clearButton.addEventListener("click", function() {
+  // Clear the search input
+  searchInput.value = "";
+
+  // Show all gallery items (reset the search)
+  galleryItems.forEach((item) => {
+    item.style.display = 'block';
+  });
+});
+
+// Function to filter results on the search results page
+function filterResults(searchTerm) {
+  // Your logic for filtering and displaying results here
+  galleryItems.forEach((item) => {
+    const itemName = item.querySelector('.projectname').textContent.toLowerCase();
+    if (itemName.includes(searchTerm)) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
